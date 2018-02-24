8dd79e42188900cf49bddd748334403499518da5
Saul-Lara <33563111+Saul-Lara@users.noreply.github.com>
2018-02-24 17:54:31 -0600

    Se añaden reglas al request

--- app/Http/Requests/CategoriaFormRequest.php
+++ app/Http/Requests/CategoriaFormRequest.php
@@ -13,7 +13,7 @@ class CategoriaFormRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class CategoriaFormRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nombre' => 'required | max: 50',
+            'descripcion' => 'max: 250'
         ];
     }
 }
